e33e3e216c5b774ca47fab6706566f3d6b39fe26
unknown <sahilsingh70526@gmail.com>
2023-02-07 13:51:42 +0530

    initial work

--- api/index.js
+++ api/index.js
@@ -1,6 +1,64 @@
 import express from 'express'
-import app from './firbase';
+import firebase from './firbase.js';
+import admin from 'firebase-admin';
 
-const app1= express();
 
-app1.use(express.urlencoded({extended:true}));
+const app= express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+admin.initializeApp({
+    credential: admin.credential.cert(firebase),
+  });
+
+  app.get("/searchUserByEmail/:email", async (req, res) => {
+    try {
+      const { email } = req.params;
+      const newUserResponse = await admin.auth().getUserByEmail(email);
+      res.json(newUserResponse);
+    } catch (error) {
+      res.json({
+        Error: error,
+      });
+    }
+  });
+
+  app.post("/sign-up", async (req, res) => {
+    try {
+      const newUserResponse = await admin.auth().createUser({
+        email: req.body.email,
+        password: req.body.password,
+        emailVerified: false,
+        disabled: false,
+      });
+      //res.status(200);
+      res.json(newUserResponse);
+    } catch (error) {
+      //res.status(404);
+      res.json({
+        Error: error,
+      });
+    }
+  });
+
+  app.post("/sign-up-new", async (req, res) => {
+    try {
+      const newUserResponse = await admin.auth().createUserWithEmailAndPassword({
+        email: req.body.email,
+        password: req.body.password,
+        emailVerified: false,
+        disabled: false,
+      });
+      //res.status(200);
+      res.json(newUserResponse);
+    } catch (error) {
+      //res.status(404);
+      res.json({
+        Error: error,
+      });
+    }
+  });
+
+  app.listen(3000,()=>{
+    console.log("server started on the port 3000")
+  });
