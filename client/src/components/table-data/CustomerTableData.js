227dd8a3e09d8c77047e127d19810ba19dc1a632
unknown <sahilsingh70526@gmail.com>
2023-02-07 18:42:06 +0530

    hfhfh

--- client/src/components/table-data/CustomerTableData.js
+++ client/src/components/table-data/CustomerTableData.js
@@ -41,7 +41,7 @@ export default function CustomerTableData({ searchedData }) {
     { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
     { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
     { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: 21 },
     { id: 6, lastName: "Melisandre", firstName: "", age: 150 },
     { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
     { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
@@ -57,21 +57,14 @@ export default function CustomerTableData({ searchedData }) {
   useEffect(() => {
     if (searchedData.length > 0) {
       const filteredData = rows.filter((customer) => {
-        if (
-          customer.firstName
-            .toLowerCase()
-            .includes(searchedData.toLowerCase())
-        ) {
+        let fullName= customer.firstName + customer.lastName;
+        if(fullName.toLowerCase().includes(searchedData.toLowerCase())){
           return true;
-        } else if (
-          customer.lastName.toLowerCase().includes(searchedData.toLowerCase())
-        ) {
-          return true;
-        } else {
+        }
+        else {
           return false;
         }
       });
-
       setData(filteredData);
     } else {
       setData(rows);
